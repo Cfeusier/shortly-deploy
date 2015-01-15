e10300d3c7911e6d48819dec6ba7ec3a8a4995b9
Clark Feusier <cfeusier@gmail.com>
2015-01-15 12:40:00 -0800

    Change production database url

--- app/config.js
+++ app/config.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
 
 var host = process.env.HOST || '127.0.0.1';
-var database = 'mongodb://MongoLab-p:LkWzgLG0L9EnAoeTRDGwX7SoKDzSeXFqC2s8O6Uygns-@ds041167.mongolab.com:41167/MongoLab-p' || 'mongodb://127.0.0.1/shortlyDevDb';
+var database = 'mongodb://127.0.0.1/shortlyDevDb';
+
+if (process.env.NODE_ENV === 'production') {
+  database = 'mongodb://MongoLab-p:LkWzgLG0L9EnAoeTRDGwX7SoKDzSeXFqC2s8O6Uygns-@ds041167.mongolab.com:41167/MongoLab-p';
+}
 
 mongoose.connect(database);
 var db = mongoose.connection;
