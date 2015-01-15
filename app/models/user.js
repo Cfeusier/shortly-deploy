5533e2961605c310e8d9ce27a98ab82bddd53adf
Clark Feusier <cfeusier@gmail.com>
2015-01-15 14:19:44 -0800

    Remove unused promise package from user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
 
 var UserSchema = new mongoose.Schema({
   username: { type: String, index: { unique: true } },
