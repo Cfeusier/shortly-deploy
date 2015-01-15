2a96f3582c909bef8e89a5280de1f7e9c0624daa
Clark Feusier <cfeusier@gmail.com>
2015-01-15 12:02:50 -0800

    Finish refactor to mongo from sql

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,21 @@
-var db = require('../config');
+var mongoose = require('mongoose');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var LinkSchema = mongoose.Schema({
+  visits: { type: Number, default: 0 },
+  url: { type: String },
+  base_url: { type: String },
+  code: { type: String },
+  title: { type: String }
+});
+
+var Link = mongoose.model('Link', LinkSchema);
+
+LinkSchema.pre('save', function(next) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
 });
 
 module.exports = Link;
